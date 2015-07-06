f91d1fb9f82e89187e7907200d46fb71ea626de4
Tiep Nguyen <tiep@tiep.info>
2015-07-06 10:31:06 +0700

    portfolio v2 init commit

--- README.md
+++ README.md
@@ -1,2 +1,68 @@
-# tiepnguyen.github.io
-Tiep's Personal Portfolio
+# Tiep Nguyen
+
+A web & mobile developer based in HCM city who specialize in simplicity and efficiency
+
+## About Me
+
+My aim is to create vibrant, detailed applications while keeping standards and usability aspects in mind
+
+### Design
+
+I use industry standard tools to help plan the full project lifecycle prior to commencement. Based on scoping, I use wireframes, user stories to create thoughtful design.
+
+### Process
+
+Every single line of code I write is manually review against established stylistic, security and procedural guidelines, automated test execution and static code analysis.
+
+### Launch
+
+By fully managed code repository and deployment system, products can be deployed reliably and securely, to ensure the switchover is as smooth as possible.
+
+## Experiences
+
+| Language		| Years	| Experience	|
+| -------------	| :---:	| :-----------:	|
+| PHP			| 5		| 95/100		|
+| HTML5			| 5		| 98/100		|
+| CSS3			| 5		| 99/100		|
+| Javascript	| 5		| 95/100		|
+| jQuery		| 4		| 96/100		|
+| Node.JS		| 3		| 85/100		|
+| Java			| 3		| 80/100		|
+| Swift			| 2		| 90/100		|
+
+## Portfolio
+
+#### Questwork PHP
+Lighting fast PHP framework for web development
+
+#### Lightness Flat CSS Style
+Flat style CSS template
+
+#### Lightness Flat jQueryUI
+Flat style theme for jQuery UI
+
+#### Lightness Mobile Framework
+Javascript framework for web-based mobile application
+
+#### Questwork Editor
+Easy & customizable WYSIWYG jQuery plugin
+
+#### Swifty Slideout Menu
+Slideover menu for iOS in Swift
+
+#### Swift HTTPRequest
+HTTPRequest wrapper for Swift
+
+#### Swift HUD
+Progress HUD for Swift
+
+#### NSManagedObject Factory
+Easier wrapper to work with NSManagedObject
+
+
+## Contact
+* Email: [tiep@tiep.info](mailto:tiep@tiep.info)
+* Skype: [vantiepnt](skype:vantiepnt)
+* Facebook: [fb.com/tiepnguyen](https://facebook.com/tiepnguyen)
+* Twitter: [@saladsmile](https://twitter.com/saladsmile)
---
+++ scripts/tiep.js
@@ -0,0 +1,77 @@
+jQuery(function($) {
+
+	var $window = $(window);
+	var htmlBody = $('html, body');
+	var masterNav = $('#master_nav');
+	var navItem = masterNav.find('a');
+	var windowHeight = $window.height();
+	var overlay = $('#overlay');
+	var dialog = $('#dialog');
+	var sections = {};
+
+	(function sectionPositions() {
+		$('section').each(function() {
+			sections[this.id] = parseInt($(this).position().top);
+		});
+	})();
+
+	$('a[rel=external]').on('click', function(e) {
+		e.preventDefault();
+		window.open(this.href);
+	})
+
+	$('#cover_scroll_btn').on('click', function() {
+		htmlBody.animate({
+			scrollTop: $('body').height()
+		}, 750, 'easeInOutExpo');
+	});
+
+	navItem.on('click', function(e) {
+		e.preventDefault();
+		if ($(this).attr('rel') == 'external') {
+			return;
+		}
+		var anchor = $(this).attr('href');
+		var des = $(anchor);
+		if (des.length) {
+			htmlBody.animate({
+				scrollTop: des.position().top
+			}, 750, 'easeInOutExpo');
+		}
+	});
+
+	$window.on('scroll', function() {
+		var windowTop = $window.scrollTop();
+		masterNav.toggleClass('fixed', windowTop > $('#profile').position().top);
+		var active;
+		for (var i in sections) {
+			if (windowTop >= sections[i] - (windowHeight / 2)) {
+				active = i;
+			}
+		}
+		navItem.removeClass('active').filter('[href=#' + active + ']').addClass('active');
+	}).on('resize', function() {
+		windowHeight = $window.height();
+		sectionPositions();
+	});
+
+	$('#contact_form').on('submit', function(e) {
+		e.preventDefault();
+		var form = $(this);
+		var fields = form.find('input, textarea, button').prop('disabled', true);
+		var submitBtn = form.find(':submit').addClass('active');
+
+		timeout = setTimeout(function() {
+			fields.val('').prop('disabled', false);
+			submitBtn.removeClass('active');
+			overlay.addClass('active');
+			dialog.addClass('jelly');
+		}, 2000);
+	});
+
+	$('#dialog .dialog-action a').on('click', function(e) {
+		overlay.removeClass('active');
+		dialog.removeClass('jelly');
+	})
+
+});
